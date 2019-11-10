7728e66abf3f7c6792f3c663e37fc18845a12762
nataliaandreas <57576857+nataliaandreas@users.noreply.github.com>
2019-11-10 20:35:34 +0000

    Add files via upload

--- js/wedding_script.js
+++ js/wedding_script.js
@@ -92,7 +92,7 @@
 	  var dataString = (arr.join (' '));
 	  $.ajax({
 	          method: "POST",
-	          url: "https://formspree.io/xnqqjkpq",
+	          url: "https://formspree.io/natalia-andreas@hotmail.com",
 	          data: dataString,
 	          dataType: "json",
 	          success: function() {
